3502e1510b7c27f47942c789141c597965255bfa
pradeep8447 <prince.chopra@smartbox.in>
2019-09-13 20:27:20 +0530

    Pass method as props in child compponent

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ChildComponent from "./ChildComponent";
 
 //// When Child ClassWat to taka data from parent class
 
 class ParentComponent extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parentName: 'Parent'
+    };
+    this.greetParent = this.greetParent.bind(this);
+  }
+
+  greetParent(childName) {
+    //Backtics ``  :::::::: Template literals
+    alert(`Hello ${this.state.parentName} from ${childName}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <ChildComponent greetHandler={this.greetParent}></ChildComponent>
+      </div>
+    );
+  }
 }
 
-export default ParentComponent
+export default ParentComponent;
